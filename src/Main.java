2291dd5aeda8be561932621c02db5751b9b53ed4
melmanss <cfhfyjdbx@gmail.com>
2025-01-27 11:33:12 +0200

    new

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Person {
+    private final String name;
+    private final int age;
+    private String profession;
+
+    public Person(String name, int age, String profession) {
+        this.name = name;
+        this.age = age;
+        this.profession = profession;
+    }
+
+    public void displayInfo() {
+        System.out.println("Ім'я: " + name + ", Вік: " + age + ", Професія: " + profession);
+    }
+
+    public void setProfession(String newProfession) {
+        this.profession = newProfession;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Person person1 = new Person("John", 30, "Інженер");
+        Person person2 = new Person("Mary", 25, "Вчитель");
+        Person person3 = new Person("Bob", 35, "Лікар");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        person1.displayInfo();
+        person2.displayInfo();
+        person3.displayInfo();
+
+        System.out.println();
+
+        Person person4 = new Person("Alice", 28, "Архітектор");
+        person4.displayInfo();
+
+        person4.setProfession("Дизайнер");
+        System.out.println("(Після оновлення професії)");
+        person4.displayInfo();
     }
 }
